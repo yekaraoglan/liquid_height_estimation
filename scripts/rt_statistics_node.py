02bb307c2502b52df5c52dfb9ce2e12bcd19fa4e
Yunus Emre Karaoğlan <yunusemrekaraoglan138@gmail.com>
2022-10-17 19:46:48 +0300

    Add Basic Moving Average Filter to smooth statistics

---
+++ scripts/rt_statistics_node.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float64MultiArray, Float64
+from geometry_msgs.msg import Point
+
+import numpy as np
+
+
+class RealtimeStatistics:
+    def __init__(self):
+        self.window_size = 100
+        
+        rospy.init_node("realtime_statistics", anonymous=False)
+        rospy.Subscriber("/height_detector/cylinder_coefficients", Float64MultiArray, self.coeff_cb)
+        self.mean_pub = rospy.Publisher("/height_detector/mean", Float64MultiArray, queue_size=1)
+        self.median_pub = rospy.Publisher("/height_detector/median", Float64MultiArray, queue_size=1)
+        self.std_pub = rospy.Publisher("/height_detector/std", Float64MultiArray, queue_size=1)
+
+        self.std_msg = self.median_msg = self.mean_msg = Float64MultiArray()
+
+        self.radius = None
+        self.x = None
+        self.y = None
+        self.z = None
+
+        self.data = np.array([])
+        self.means = np.zeros((1,4))
+        self.medians = np.zeros((1,4))
+        self.stds = np.zeros((1,4))
+
+    def coeff_cb(self, msg):
+        self.x = msg.data[0]
+        self.y = msg.data[1]
+        self.z = msg.data[2]
+        self.radius = msg.data[6]
+        arr = np.array([self.x, self.y, self.z, self.radius]).reshape(1,4)
+        if self.data.size == 0:
+            self.data = arr
+        else:
+            self.data = np.vstack([self.data, arr])
+        if self.data.shape[0] >= self.window_size:
+            self.data = self.data[-self.window_size:, :]
+            self.means = np.mean(self.data, axis=0)
+            self.medians = np.median(self.data, axis=0)
+            self.stds = np.std(self.data, axis=0)
+        else:
+            self.means = np.mean(self.data, axis=0)
+            self.medians = np.median(self.data, axis=0)
+            self.stds = np.std(self.data, axis=0)
+        self.mean_msg.data = self.means
+        self.mean_pub.publish(self.mean_msg)
+        self.median_msg.data = self.medians
+        self.median_pub.publish(self.median_msg)
+        self.std_msg.data = self.stds
+        self.std_pub.publish(self.std_msg)
+
+if __name__ == "__main__":
+    try:
+        RealtimeStatistics()
+        rospy.spin()
+    except KeyboardInterrupt:
+        rospy.signal_shutdown("Keyboard Interrupt!")
